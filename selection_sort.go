73f6b2c3a97610e55599abf64d8b9079e07e6a86
heyanfu <1145291570@qq.com>
2019-04-26 21:11:16 +0800

    选择排序

---
+++ selection_sort.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{5, 4, 3, 2, 1, 6, 7, 8, 9, 10}
+	selectionSort(a)
+	for _, v := range a {
+		fmt.Println(v)
+	}
+}
+
+func selectionSort(a []int) {
+	length := len(a)
+	for i := 0; i < length; i++ {
+		minIndex := i
+		for j := i + 1; j < length; j++ {
+			if a[minIndex] > a[j] {
+				minIndex = j
+			}
+		}
+		a[minIndex], a[i] = a[i], a[minIndex]
+	}
+}
